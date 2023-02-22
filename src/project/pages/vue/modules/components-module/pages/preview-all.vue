2ef6b08f3be18aaf94ba3cfe67246c1d4b584084
HBO30 <diab.4869@gmail.com>
2023-02-22 21:13:30 +0200

    feat : components preview

---
+++ src/project/pages/vue/modules/components-module/pages/preview-all.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div>
+    <v-card>
+        <v-card-text>
+            <applyForm></applyForm>
+        </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import applyForm from '../components/local-apply-form-component'
+export default {
+    components: {
+        applyForm
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/project/pages/vue/modules/router.js
@@ -0,0 +1,9 @@
+export default {
+    path: 'comp',
+    component: () => import('./module-layout.vue'),
+    children: [
+        
+    ]
+
+    
+}
