e34a789c80c905906cf29f3ee4b892301dc8b9c7
HBO30 <diab.4869@gmail.com>
2023-02-23 20:23:19 +0200

    chore : preview components

--- src/project/pages/vue/pages/home/home-page.vue
+++ src/project/pages/vue/pages/home/home-page.vue
@@ -3,6 +3,9 @@
     <m-hero> </m-hero>
     <m-media-feed> </m-media-feed>
     <m-contact></m-contact>
+    <m-apply-form></m-apply-form>
+    <m-open-position-form></m-open-position-form>
+    <m-b-list-input></m-b-list-input>
     <m-footer> </m-footer>
   </div>
 </template>
@@ -12,11 +15,17 @@ import mHero from "./components/hero.vue";
 import mMediaFeed from "./components/media-feed.vue";
 import mContact from "./components/contact.vue";
 import mFooter from "./components/footer.vue";
+import mApplyForm from './components/apply-form.vue'
+import mOpenPositionForm from './components/open-position-form.vue'
+import mBListInput from './components/b-list-input.vue'
 export default {
   components: {
     "m-hero": mHero,
     "m-media-feed": mMediaFeed,
     "m-contact": mContact,
+    "m-apply-form": mApplyForm,
+    "m-open-position-form": mOpenPositionForm,
+    "m-b-list-input": mBListInput,
     "m-footer": mFooter,
   },
   data() {
