e98f5ae081ae76fbb296c647ca5f8ffe159377b6
HBO30 <diab.4869@gmail.com>
2023-02-24 19:15:25 +0200

    chore : apply page structure

--- src/project/pages/website/pages/apply/index.vue
+++ src/project/pages/website/pages/apply/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>apply page</div>
+  <div>
+    <local-page-wrapper>
+      <local-page-header> </local-page-header>
+      <local-px >
+        <local-apply-form> </local-apply-form>
+      </local-px>
+    </local-page-wrapper>
+  </div>
 </template>
 <script>
-export default {};
+import pageWrapper from "./components/page-wrapper.vue";
+import pageHeader from "./components/page-header.vue";
+import applyForm from "./components/apply-form.vue";
+import px from "./components/px.vue";
+export default {
+  components: {
+    "local-page-wrapper": pageWrapper,
+    "local-page-header": pageHeader,
+    "local-apply-form": applyForm,
+    "local-px": px,
+  },
+};
 </script>
