687844ef7ffed0effebfd3de506853267f1f2d14
HBO30 <diab.4869@gmail.com>
2023-02-24 19:16:30 +0200

    feat : apply page header

---
+++ src/project/pages/website/pages/apply/components/page-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="py-5 px-4">
+    <div class="text-h4">
+        Front End 
+    </div>
+    <div class="text-title-body">
+        E00093
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
