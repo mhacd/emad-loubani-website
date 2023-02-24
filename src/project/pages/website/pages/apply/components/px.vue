b246e589a13c074f3dcf3adcb63f85f0edbeb56e
HBO30 <diab.4869@gmail.com>
2023-02-24 19:17:18 +0200

    feat : spacing components. px component

---
+++ src/project/pages/website/pages/apply/components/px.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="`px-${forceX ? forceX : x}`">
+  <slot></slot></div>
+</template>
+
+<script>
+export default {
+  props: {
+    forceX: {
+      type: Number,
+    },
+  },
+  computed: {
+    x() {
+      return 3;
+    },
+  },
+};
+</script>
+
+<style></style>
