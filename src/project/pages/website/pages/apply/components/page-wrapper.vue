0c85c83d5c11c79eeea65ee04e398f88430f9ec2
HBO30 <diab.4869@gmail.com>
2023-02-24 19:16:56 +0200

    feat : page wrapper. apply page

---
+++ src/project/pages/website/pages/apply/components/page-wrapper.vue
@@ -0,0 +1,22 @@
+<template>
+ <v-container fluid>
+      <v-row>
+        <v-col>
+            <v-sheet  max-width="700" class="mx-auto rounded-xl">
+                <slot></slot>
+            </v-sheet>
+        </v-col>
+      </v-row>  
+    </v-container>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
